a5d545c00b844c71c83383525ad576d5b2b04e99
yoshw <yoshwakeham@gmail.com>
2015-02-04 10:29:16 +1100

    Move make_path_unless_exists to utilities

--- utilities.py
+++ utilities.py
@@ -20,7 +20,11 @@ The module is designed to be used with Python 2.7.x.
 Change Log
 ----------
 4 Feb 2015: created secs_to_hms() utility
+            moved directory creation utility here
 """
+
+import os
+import errno
 
 
 def secs_to_hms(time_delta):
@@ -28,3 +32,12 @@ def secs_to_hms(time_delta):
     mins, secs = divmod(time_delta, 60)
     hours, mins = divmod(mins, 60)
     return "{:02d}:{:02d}:{:.1f}".format(int(hours), int(mins), secs)
+
+
+def make_path_unless_exists(path):
+    """Make a directory, unless it already exists"""
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
