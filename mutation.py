480625f27c45521caafc26ca02fc36588c230aed
Yoshua Wakeham <yoshwakeham@gmail.com>
2015-02-26 15:21:21 +1100

    Initial commit for Mutation class

---
+++ mutation.py
@@ -0,0 +1,11 @@
+"""
+Module for class representing mutations,
+and associated functions.
+"""
+
+class Mutation(object):
+    """
+    Class to represent mutations.
+    """
+    def __init__(self):
+        pass
