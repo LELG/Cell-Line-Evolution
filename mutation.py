c011896c06888d268b218148ecfc0a8d44486406
Yoshua Wakeham <yoshwakeham@gmail.com>
2015-02-26 15:43:11 +1100

    Work on Mutation class

--- mutation.py
+++ mutation.py
@@ -2,10 +2,23 @@
 Module for class representing mutations,
 and associated functions.
 """
+import math
+import random
 
 class Mutation(object):
     """
     Class to represent mutations.
     """
-    def __init__(self):
-        pass
+    def __init__(self, opt, mut_id=None):
+        """Create new mutation"""
+        if not mut_id:
+            self.mut_id = id(self)
+        self.prolif_rate_effect = get_prolif_rate_mutn(opt)
+
+def get_prolif_rate_mutn(opt):
+    """Generate a proliferation rate mutation effect."""
+    mutn_magnitude = opt.sample_from_beta_dist() * opt.scale
+    sample_for_mut_type = random.random() - opt.prob_del_mut
+    mutn_valence = math.copysign(1, sample_for_mut_type)
+    return mutn_magnitude * mutn_valence
+
